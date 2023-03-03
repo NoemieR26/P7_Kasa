c7ef75bb9df7620d0c1b5482f1ec70f453fd2730
Noémie <noemie.roynette@gmail.com>
2023-03-03 09:46:34 +0100

    NavLink Card

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,16 +1,17 @@
+import { NavLink } from 'react-router-dom'
 import"./Home.css";
 
 export default function Card ({ id, title, cover }) {
     return(
         <li key={id} className="housing">
-            <a href={"/housing/" + id}>
+            <NavLink title="Housing" end to={"/housing/" + id}>
                 <div className="housing_card">
                     <img src={cover} alt={title}  className="housing_card_cover"/>
                     <div className="housing_card_content">
                         <h2 className="housing_card_content_title">{title}</h2>
                     </div>
                 </div>
-            </a>
+            </NavLink>
         </li>
     )
 }
